70afd070f9d4493ac5af328c03162db716882e0c
muhammadali78907ijj-sudo <muhammadali78907ijj@gmail.com>
2026-04-15 10:23:02 +0500

    Add Python script to print elements and filter numbers

---
+++ Python_work.py
@@ -0,0 +1,10 @@
+nums=[5,10,15,20,25]
+print("first element",nums[0])
+print("last element",nums[4])
+nums=[5,10,15,20,25]
+for i in nums:
+    print(1)
+print("numbers greater than 10:")
+for i in nums:
+    if i >10:
+    print(1)
